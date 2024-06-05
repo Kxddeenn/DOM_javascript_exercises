0bc20bbe40d6010cd479c95e90a7c552813e705b
Kxddeenn <kaden.neild@gmail.com>
2024-06-05 17:06:10 -0400

    DOM exercises final

--- script.js
+++ script.js
@@ -23,3 +23,23 @@ container.appendChild(paragraph);
  blueText.textContent = "I'm a blue h3!";
  container.appendChild(blueText);
 
+/* a <div> with a black border and pink background color with the following elements inside of it:
+another <h1> that says “I’m in a div”
+a <p> that says “ME TOO!”
+Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
+*/
+
+const example = document.createElement("div");
+example.style.backgroundColor = "pink";
+example.style.border = "black";
+
+const header = document.createElement("h1");
+header.textContent = "I'm in a div";
+
+const paragraph2 = document.createElement("p");
+paragraph2.textContent = "ME TOO!";
+
+example.appendChild(header);
+example.appendChild(paragraph2);
+
+container.appendChild(example);
